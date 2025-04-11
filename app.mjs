e7a6b3d0059ba33f65784b26ed446d7ef6cb6593
parinTNK <parin.tnk@gmail.com>
2025-04-11 09:57:37 +0700

    feat: setup project with database connection and installed Prisma as the ORM for database management.

--- app.mjs
+++ app.mjs
@@ -1,13 +1,27 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
+
+(async () => {
+  try {
+    const client = await connectionPool.connect();
+    console.log("Connected to PostgreSQL database");
+    client.release();
+  } catch (error) {
+    console.error("Error connecting to PostgreSQL database", error);
+  }
+}
+)();
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
